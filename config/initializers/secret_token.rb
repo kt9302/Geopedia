eb8150a54402da35764dce9ee00e5b0d8fbe937f
Tian <jasontiank@gmail.com>
2013-11-30 18:05:27 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geopedia::Application.config.secret_key_base = '68dbf8ab39d0ee43e3dc048ce9d40b4ba5cc75f36336d2b0e8c6d0c820c5202beb62e3d09bcc1d04a54c4eee6c810de2a55b34ce38fc9ebd44aeda685f15e3ae'
